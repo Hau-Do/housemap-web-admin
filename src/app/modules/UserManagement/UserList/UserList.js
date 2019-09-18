761b8e4bc66a90feefcd0b1958e38c3f40b7e1e8
Peter <peter@Dos-MacBook-Air.local>
2019-09-18 11:48:44 +0700

    use react Hook in the project

--- src/app/modules/UserManagement/UserList/UserList.js
+++ src/app/modules/UserManagement/UserList/UserList.js
@@ -1,11 +1,33 @@
-import React from 'react';
-
 import './UserList.scss';
+import React, { useState, useEffect } from 'react';
+import { getUserList } from '../../../services/UserService';
+
+
 
 const UserList = () => {
 
+    const [users, setUsers] = useState([]);  
+    
+    useEffect(() => {
+        getUsers();
+        async function getUsers() {
+            const res = await getUserList();
+            if(res && res.data) {
+                setUsers(res.data);
+                console.log('OK');
+            }
+        };
+        
+    })
+
     return(
-        <div id="user-list">User List</div>
+        <div id="user-list">
+            <h2>User List</h2>
+            { users.map(user => (
+                <div>{user.name}</div>
+            ))}
+        </div>
+       
     );
 };
 
